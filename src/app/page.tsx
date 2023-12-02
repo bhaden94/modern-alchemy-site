a6a617f38c58b03b66cea2aee3ceff2f2d923649
bhaden94 <bhaden94@yahoo.com>
2023-12-02 09:08:02 -0500

    clan up imports.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import { getClient } from '~/lib/sanity/sanity.client'
-import { getPosts } from '~/lib/sanity/sanity.queries'
-
 import Home from '../components/Home'
 
 export default async function RootPage() {
