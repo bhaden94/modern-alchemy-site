388d0c58746780fe0c26afedd23ad5d332f79ac9
bhaden94 <bhaden94@yahoo.com>
2023-12-02 09:01:39 -0500

    Add media tool to sanity studio.

--- sanity.config.ts
+++ sanity.config.ts
@@ -11,6 +11,7 @@ import {
   IframeOptions,
 } from 'sanity-plugin-iframe-pane'
 import { previewUrl } from 'sanity-plugin-iframe-pane/preview-url'
+import { media } from 'sanity-plugin-media'
 
 // see https://www.sanity.io/docs/api-versioning for how versioning works
 import {
@@ -64,5 +65,6 @@ export default defineConfig({
     // Vision lets you query your content with GROQ in the studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({ defaultApiVersion: apiVersion }),
+    media(),
   ],
 })
