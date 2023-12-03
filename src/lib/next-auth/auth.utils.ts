6e67dec9693c8a64c08fe1d184c44acad569d93b
bhaden94 <bhaden94@yahoo.com>
2023-12-03 09:40:13 -0500

    Move more over to app router.

--- src/lib/next-auth/auth.utils.ts
+++ src/lib/next-auth/auth.utils.ts
@@ -2,6 +2,8 @@ import { JWT } from 'next-auth/jwt'
 
 import { getClient } from '../sanity/sanity.client'
 import { getAllowedUsers } from '../sanity/sanity.queries'
+import { NextAuthOptions } from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
 
 export const REDIRECT_URL = 'redirectUrl'
 export const AUTHORIZED_ROLE = 'authorizedUser'
@@ -15,4 +17,35 @@ export const checkIfAuthorized = async (token: JWT): Promise<boolean> => {
   const allowedUsers = await getAllowedUsers(authClient)
 
   return allowedUsers.some((user) => user?.email === token?.email)
+}
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_AUTH_ID || '',
+      clientSecret: process.env.GOOGLE_AUTH_SECRET || '',
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    async jwt({ token }) {
+      const isAuthorized = await checkIfAuthorized(token)
+
+      if (isAuthorized) {
+        token.role = UserRoles.ADMIN
+      }
+
+      return token
+    },
+  },
 }
