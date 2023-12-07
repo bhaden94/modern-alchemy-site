41a8d1429a3eb96a617a12269f11cf57ecdf87a3
bhaden94 <bhaden94@yahoo.com>
2023-12-07 16:59:13 -0500

    Setup form validation with zod.

--- src/components/TattooForm.tsx
+++ src/components/TattooForm.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
-import { useForm } from 'react-hook-form'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { TBookingSchema, bookingSchema } from '~/utils/bookingFormUtils'
 
 // TODO: split into components
 // TODO: implement reCAPTCHA for form submission
@@ -10,9 +12,9 @@ const TattooForm = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm()
+  } = useForm<TBookingSchema>({ resolver: zodResolver(bookingSchema) })
 
-  const onSubmit = async (data) => {
+  const onSubmit: SubmitHandler<TBookingSchema> = async (data) => {
     setIsSubmitting(true)
     const formData = new FormData()
     const images: FileList = data.showcaseImages
@@ -44,88 +46,63 @@ const TattooForm = () => {
     <form onSubmit={handleSubmit(onSubmit)}>
       {/* Name */}
       <label htmlFor="name">Name:</label>
-      <input type="text" id="name" {...register('name', { required: true })} />
+      <input type="text" id="name" {...register('name')} />
       {errors.name && <span>This field is required</span>}
 
       {/* Phone Number */}
-      <label htmlFor="phone_number">Phone Number:</label>
-      <input
-        type="tel"
-        id="phone_number"
-        {...register('phone_number', { required: true, pattern: /^\d{10}$/ })}
-      />
-      {errors.phone_number && <span>Please enter a valid phone number</span>}
+      <label htmlFor="phoneNumber">Phone Number:</label>
+      <input type="tel" id="phoneNumber" {...register('phoneNumber')} />
+      {errors.phoneNumber && <span>Please enter a valid phone number</span>}
 
       {/* Email */}
       <label htmlFor="email">Email:</label>
-      <input
-        type="email"
-        id="email"
-        {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
-      />
+      <input type="email" id="email" {...register('email')} />
       {errors.email && <span>Please enter a valid email address</span>}
 
       {/* Characters */}
       <label htmlFor="characters">Characters:</label>
-      <input
-        type="text"
-        id="characters"
-        {...register('characters', { required: true })}
-      />
+      <input type="text" id="characters" {...register('characters')} />
       {errors.characters && <span>This field is required</span>}
 
       {/* Description */}
       <label htmlFor="description">Description:</label>
-      <textarea
-        id="description"
-        {...register('description', { required: true })}
-      />
+      <textarea id="description" {...register('description')} />
       {errors.description && <span>This field is required</span>}
 
       {/* Location */}
       <label htmlFor="location">Location:</label>
-      <input
-        type="text"
-        id="location"
-        {...register('location', { required: true })}
-      />
+      <input type="text" id="location" {...register('location')} />
       {errors.location && <span>This field is required</span>}
 
       {/* Style */}
       <label htmlFor="style">Style:</label>
-      <select id="style" {...register('style', { required: true })}>
+      <select id="style" {...register('style')}>
         <option value="color">Color</option>
         <option value="black_and_grey">Black and Grey</option>
       </select>
       {errors.style && <span>This field is required</span>}
 
       {/* Prior Tattoo */}
-      <label htmlFor="prior_tattoo">Prior Tattoo:</label>
-      <select
-        id="prior_tattoo"
-        {...register('prior_tattoo', { required: true })}
-      >
+      <label htmlFor="priorTattoo">Prior Tattoo:</label>
+      <select id="priorTattoo" {...register('priorTattoo')}>
         <option value="new_tattoo">Yes - I want a new tattoo</option>
         <option value="ongoing_project">
           Yes - this is an ongoing project
         </option>
         <option value="no">No</option>
       </select>
-      {errors.prior_tattoo && <span>This field is required</span>}
+      {errors.priorTattoo && <span>This field is required</span>}
 
       {/* Preferred Day */}
-      <label htmlFor="preferred_day">Preferred Day:</label>
-      <select
-        id="preferred_day"
-        {...register('preferred_day', { required: true })}
-      >
+      <label htmlFor="preferredDay">Preferred Day:</label>
+      <select id="preferredDay" {...register('preferredDay')}>
         <option value="monday">Monday</option>
         <option value="tuesday">Tuesday</option>
         <option value="wednesday">Wednesday</option>
         <option value="thursday">Thursday</option>
         <option value="friday">Friday</option>
       </select>
-      {errors.preferred_day && <span>This field is required</span>}
+      {errors.preferredDay && <span>This field is required</span>}
 
       {/* Showcase Images */}
       {/* Need to make this a better experience and obvious multiple uploads can be done */}
@@ -134,9 +111,9 @@ const TattooForm = () => {
         type="file"
         id="showcaseImages"
         multiple
-        {...register('showcaseImages', { required: true })}
+        {...register('showcaseImages')}
       />
-      {errors.showcaseImages && <span>This field is required</span>}
+      {errors.showcaseImages && <span>{errors.showcaseImages.message}</span>}
 
       {/* Submit button */}
       <button type="submit" disabled={isSubmitting}>
---
+++ src/utils/bookingFormUtils.ts
@@ -0,0 +1,67 @@
+import z from 'zod'
+
+const phoneRegex = new RegExp(
+  /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/,
+)
+
+const MAX_FILE_SIZE = 10485760 // 10MB
+const ACCEPTED_IMAGE_TYPES = [
+  'image/jpeg',
+  'image/jpg',
+  'image/png',
+  'image/webp',
+]
+
+export const bookingSchema = z.object({
+  name: z.string(),
+  phoneNumber: z.string().regex(phoneRegex, 'Invalid phone number'),
+  email: z.string().email({ message: 'Invalid email address' }),
+  characters: z.string(),
+  description: z.string(),
+  location: z.string(),
+  style: z.string(),
+  priorTattoo: z.string(),
+  preferredDay: z.string(),
+  showcaseImages: z.custom<FileList>().superRefine((files, ctx) => {
+    if (files.length === 0) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'File must be provided',
+      })
+      return false
+    }
+
+    if (files.length > 5) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'A maximum of 5 images can be uploaded',
+      })
+      return false
+    }
+
+    for (let i = 0; i < files.length; i++) {
+      if (!ACCEPTED_IMAGE_TYPES.includes(files[i].type)) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: 'File must be a valid image type',
+        })
+        return false
+      }
+    }
+
+    for (let i = 0; i < files.length; i++) {
+      if (files[i].size > MAX_FILE_SIZE) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          message: 'Each file must be less than 10MB',
+        })
+        return false
+      }
+    }
+
+    return true
+  }),
+})
+
+// extracting the type
+export type TBookingSchema = z.infer<typeof bookingSchema>
