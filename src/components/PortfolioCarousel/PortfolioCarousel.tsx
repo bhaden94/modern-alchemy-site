1188062e979827ff25d89c8ebc298a36d3369751
bhaden94 <bhaden94@yahoo.com>
2024-01-01 19:44:33 -0500

    Listen to carousel scroll changes and set selected thumbnail.

--- src/components/PortfolioCarousel/PortfolioCarousel.tsx
+++ src/components/PortfolioCarousel/PortfolioCarousel.tsx
@@ -2,25 +2,35 @@
 
 import { Carousel, Embla } from '@mantine/carousel'
 import Image from 'next/image'
-import { useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { ImageAsset } from 'sanity'
 
 const PortfolioCarousel = ({ images }: { images: { asset: ImageAsset }[] }) => {
   const [embla, setEmbla] = useState<Embla | null>(null)
   const [thumbIndex, setThumbIndex] = useState<number>(0)
 
-  const setBorderColor = (i: number): string => {
-    return i === thumbIndex
-      ? 'border-[var(--mantine-primary-color-filled)]'
-      : ''
-  }
-
-  const scrollTo = (index: number) => {
+  const onSelect = useCallback(() => {
     if (embla) {
-      embla.scrollTo(index)
-      setThumbIndex(index)
+      setThumbIndex(embla.selectedScrollSnap())
     }
-  }
+  }, [embla, setThumbIndex])
+
+  const scrollTo = useCallback(
+    (index: number) => {
+      if (embla) {
+        embla.scrollTo(index)
+        setThumbIndex(index)
+      }
+    },
+    [embla],
+  )
+
+  useEffect(() => {
+    if (embla) {
+      embla.on('select', onSelect)
+      embla.on('reInit', onSelect)
+    }
+  }, [embla, onSelect])
 
   return (
     <div className="flex flex-wrap">
@@ -38,6 +48,7 @@ const PortfolioCarousel = ({ images }: { images: { asset: ImageAsset }[] }) => {
                 src={image.asset.url}
                 alt="Portfolio image thumbnail"
                 fill
+                sizes="100%"
                 style={{
                   objectFit: 'contain',
                 }}
@@ -49,9 +60,11 @@ const PortfolioCarousel = ({ images }: { images: { asset: ImageAsset }[] }) => {
       <div className="flex gap-2 w-full">
         {images?.map((image, i) => (
           <Image
-            className={`${setBorderColor(
-              i,
-            )} rounded-md border-solid cursor-pointer transition duration-300 ease-in-out hover:opacity-50`}
+            className={`${
+              i === thumbIndex
+                ? 'border-[var(--mantine-primary-color-filled)]'
+                : ''
+            } rounded-md border-solid cursor-pointer transition duration-300 ease-in-out hover:opacity-50`}
             src={image.asset.url}
             key={image.asset.originalFilename}
             alt="Portfolio image thumbnail"
