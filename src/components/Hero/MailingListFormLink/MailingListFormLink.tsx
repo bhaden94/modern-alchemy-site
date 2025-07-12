ad6edb04cca05e17b55dbfe93d19008c9ae54a01
bhaden94 <bhaden94@yahoo.com>
2025-07-12 16:54:28 -0400

    Add small hover animation to arrow ni mailing list link.

--- src/components/Hero/MailingListFormLink/MailingListFormLink.tsx
+++ src/components/Hero/MailingListFormLink/MailingListFormLink.tsx
@@ -4,6 +4,8 @@ import { IconArrowDown } from '@tabler/icons-react'
 import { MailingListFormContent } from '~/schemas/pages/mailingListFormContent'
 import { NavigationInternalIds } from '~/utils/navigation'
 
+import classes from './MailingListFormLink.module.css'
+
 interface IMailingListFormLink {
   content?: MailingListFormContent
 }
@@ -17,12 +19,14 @@ const MailingListFormLink = ({ content }: IMailingListFormLink) => {
       underline="hover"
       c="white"
       fw="bold"
+      className={classes.link}
     >
       <Group gap={1} wrap="nowrap">
         {content.heroTitle || 'Join our mailing list'}
-        <IconArrowDown />
+        <IconArrowDown className={classes.arrowIcon} />
       </Group>
     </Anchor>
   )
 }
+
 export default MailingListFormLink
